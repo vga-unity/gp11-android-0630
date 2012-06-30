f56c06f6381b23c2737839a2d73923459c0d8963
shingo tanaka <shingo.tanaka.0808@gmail.com>
2012-06-30 12:39:25 +0900

    提出バージョンです

---
+++ Assets/Scripts/Generator.js
@@ -0,0 +1,19 @@
+#pragma strict
+
+var prefab : GameObject;
+
+function Start () {
+    while(true) {
+        var position = Vector3(Random.Range(-100.0, 100.0), 50.0, Random.Range(-100.0, 100.0));
+
+        var hit : RaycastHit;
+        Physics.Raycast(position, Vector3.up * -1.0, hit);
+
+        var instance : GameObject = Instantiate(prefab, hit.point, Quaternion.identity);
+
+        while(instance!=null) yield;
+    }
+
+    
+}
+
