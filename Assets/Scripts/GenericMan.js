dd90f00b88fc230dd3eb6799d9d0d8907c2c72d6
Eri Satsuki <neige.lune.fleur.19@gmail.com>
2012-06-30 12:39:06 +0900

    提出バージョンです

---
+++ Assets/Scripts/GenericMan.js
@@ -0,0 +1,16 @@
+#pragma strict
+
+var ragdollPrefab : GameObject;
+
+function OnTriggerEnter(collider : Collider) {
+    if (collider.gameObject.name == "Body"){
+        var ragdoll : GameObject =
+         Instantiate(ragdollPrefab,transform.position,transform.rotation);
+        var body =
+         ragdoll.GetComponentInChildren.<Rigidbody>();
+
+        body.AddForce(Vector3.up * 150.0, ForceMode.Impulse);
+
+        Destroy(gameObject);
+    }
+}
