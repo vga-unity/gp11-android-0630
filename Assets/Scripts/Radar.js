42afaa88575f8c34d4dbf0ebb88b98d99662ebaa
keijiro <keijiro@gmail.com>
2012-06-30 14:59:12 +0900

    授業中に作成したバージョン

---
+++ Assets/Scripts/Radar.js
@@ -0,0 +1,12 @@
+#pragma strict
+
+private var target : GameObject;
+
+function Update() {
+	if (target == null) {
+		target = GameObject.Find("GenericMan(Clone)");		
+	}
+	if (target != null) {
+		transform.LookAt(target.transform.position);
+	}
+}
